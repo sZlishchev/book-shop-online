5548abfe1ea5d841e43d88b4ce34a93e6534e5d2
Stas Zlishchev <zlishchev@gmail.com>
2023-08-15 15:42:29 +0300

    Main structure

---
+++ src/main/java/shop/online/bookshoponline/BookShopOnlineApplication.java
@@ -0,0 +1,13 @@
+package shop.online.bookshoponline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookShopOnlineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookShopOnlineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/shop/online/bookshoponline/BookShopOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.online.bookshoponline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookShopOnlineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
